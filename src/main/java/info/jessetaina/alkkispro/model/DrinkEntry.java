2f29bfae71017b27fc04733525d912dd33d60204
Jesse Taina <jtaina@gmail.com>
2018-04-16 19:11:25 +0300

    drink entry repo

---
+++ src/main/java/info/jessetaina/alkkispro/model/DrinkEntry.java
@@ -0,0 +1,74 @@
+package info.jessetaina.alkkispro.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class DrinkEntry {
+
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO)
+	private long drink_entry_id;
+	
+	@NotNull private Date drink_date;
+	
+	@NotNull
+	@ManyToOne(targetEntity = Juoma.class, optional = false)
+	@JoinColumn
+	private long juoma_id;
+	
+	
+	@NotNull private int drink_quantity; 
+	@NotNull private Double drink_entry_units;
+	
+	public DrinkEntry() {}
+	
+	public DrinkEntry(long drink__entry_id, @NotNull Date drink_date, @NotNull long drink_id,
+			@NotNull int drink_quantity, @NotNull Double drink_entry_units) {
+		super();
+		this.drink_entry_id = drink__entry_id;
+		this.drink_date = drink_date;
+		this.juoma_id = drink_id;
+		this.drink_quantity = drink_quantity;
+		this.drink_entry_units = drink_entry_units;
+	}
+	public long getDrink__entry_id() {
+		return drink_entry_id;
+	}
+	public void setDrink__entry_id(long drink__entry_id) {
+		this.drink_entry_id = drink__entry_id;
+	}
+	public Date getDrink_date() {
+		return drink_date;
+	}
+	public void setDrink_date(Date drink_date) {
+		this.drink_date = drink_date;
+	}
+	public long getDrink_id() {
+		return juoma_id;
+	}
+	public void setDrink_id(long drink_id) {
+		this.juoma_id = drink_id;
+	}
+	public int getDrink_quantity() {
+		return drink_quantity;
+	}
+	public void setDrink_quantity(int drink_quantity) {
+		this.drink_quantity = drink_quantity;
+	}
+	public Double getDrink_entry_units() {
+		return drink_entry_units;
+	}
+	public void setDrink_entry_units(Double drink_entry_units) {
+		this.drink_entry_units = drink_entry_units;
+	}
+
+	
+}
---
+++ src/main/java/info/jessetaina/alkkispro/model/DrinkEntryRepository.java
@@ -0,0 +1,7 @@
+package info.jessetaina.alkkispro.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DrinkEntryRepository extends CrudRepository<DrinkEntry, Long>{
+
+}
