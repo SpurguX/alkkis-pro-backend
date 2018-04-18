e8c7d6142414a7a097861e91c22823b9e0c97ce8
Jesse Taina <jtaina@gmail.com>
2018-04-18 10:31:56 +0300

    DrinkEntry entity now connected to database and drink table

--- src/main/java/info/jessetaina/alkkispro/model/DrinkEntry.java
+++ src/main/java/info/jessetaina/alkkispro/model/DrinkEntry.java
@@ -2,12 +2,15 @@ package info.jessetaina.alkkispro.model;
 
 import java.util.Date;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -17,33 +20,38 @@ public class DrinkEntry {
 	@GeneratedValue (strategy=GenerationType.AUTO)
 	private long drink_entry_id;
 	
-	@NotNull private Date drink_date;
+	@NotNull 
+	@Basic
+	@Temporal(TemporalType.DATE)
+	private Date drink_date;
 	
 	@NotNull
-	@ManyToOne(targetEntity = Juoma.class, optional = false)
-	@JoinColumn
-	private long juoma_id;
+	@ManyToOne
+	@JoinColumn(name = "drink_id")
+	private Juoma juoma;
 	
+	@NotNull 
+	private int drink_quantity; 
 	
-	@NotNull private int drink_quantity; 
-	@NotNull private Double drink_entry_units;
+	@NotNull 
+	private Double drink_entry_units;
 	
 	public DrinkEntry() {}
 	
-	public DrinkEntry(long drink__entry_id, @NotNull Date drink_date, @NotNull long drink_id,
+	public DrinkEntry(long drink_entry_id, @NotNull Date drink_date, @NotNull Juoma juoma,
 			@NotNull int drink_quantity, @NotNull Double drink_entry_units) {
 		super();
-		this.drink_entry_id = drink__entry_id;
+		this.drink_entry_id = drink_entry_id;
 		this.drink_date = drink_date;
-		this.juoma_id = drink_id;
+		this.juoma = juoma;
 		this.drink_quantity = drink_quantity;
 		this.drink_entry_units = drink_entry_units;
 	}
-	public long getDrink__entry_id() {
+	public long getDrink_entry_id() {
 		return drink_entry_id;
 	}
-	public void setDrink__entry_id(long drink__entry_id) {
-		this.drink_entry_id = drink__entry_id;
+	public void setDrink_entry_id(long drink_entry_id) {
+		this.drink_entry_id = drink_entry_id;
 	}
 	public Date getDrink_date() {
 		return drink_date;
@@ -51,11 +59,11 @@ public class DrinkEntry {
 	public void setDrink_date(Date drink_date) {
 		this.drink_date = drink_date;
 	}
-	public long getDrink_id() {
-		return juoma_id;
+	public Juoma getJuoma() {
+		return juoma;
 	}
-	public void setDrink_id(long drink_id) {
-		this.juoma_id = drink_id;
+	public void setJuoma(Juoma juoma) {
+		this.juoma = juoma;
 	}
 	public int getDrink_quantity() {
 		return drink_quantity;
