cb0da2fe0d45c99670afd5a04e5a96fc09cb08c9
Taina Jesse <jesse.taina@hiq.fi>
2022-06-13 10:21:03 +0300

    Add enum DrinkType and add it to the Drink class. Improve formatting.

--- src/main/java/info/jessetaina/alkkispro/controller/AlkkisController.java
+++ src/main/java/info/jessetaina/alkkispro/controller/AlkkisController.java
@@ -19,71 +19,73 @@ import info.jessetaina.alkkispro.model.Drink;
 
 @Controller
 public class AlkkisController {
-	
 
 	@Autowired
 	private DrinkRepository drinkRepository;
 	@Autowired
 	private DrinkEntryRepository drinkEntryRepository;
 
-	@RequestMapping(value = "/add_drink", method=RequestMethod.POST)
-	public @ResponseBody Drink addDrink(HttpServletRequest request, @RequestParam(value="drink_name") String name, 
-			@RequestParam(value="volume") Double volume, @RequestParam(value="alc_content") Double alc_content, @RequestParam(value="units") Double units) {
-		Drink newDrink = new Drink(name, volume, alc_content, units, true);
+	@RequestMapping(value = "/add_drink", method = RequestMethod.POST)
+	public @ResponseBody Drink addDrink(HttpServletRequest request, @RequestParam(value = "drink_name") String name,
+			@RequestParam(value = "volume") Double volume, @RequestParam(value = "alc_content") Double alc_content,
+			@RequestParam(value = "units") Double units, @RequestParam(value = "type") Drink.DrinkType type) {
+		Drink newDrink = new Drink(name, volume, alc_content, units, true, type);
 		drinkRepository.save(newDrink);
 		return newDrink;
 	}
-	
-	@RequestMapping(value = "/save_other_drink", method=RequestMethod.POST)
-	public @ResponseBody Drink saveOtherDrink(HttpServletRequest request, @RequestParam(value="drinkName") String name, 
-			@RequestParam(value="volume") Double volume, @RequestParam(value="alcContent") Double alc_content, @RequestParam(value="units") Double units) {
-		Drink newSavedDrink = new Drink(name, volume, alc_content, units, false);
+
+	@RequestMapping(value = "/save_other_drink", method = RequestMethod.POST)
+	public @ResponseBody Drink saveOtherDrink(HttpServletRequest request, @RequestParam(value = "drinkName") String name,
+			@RequestParam(value = "volume") Double volume, @RequestParam(value = "alcContent") Double alc_content,
+			@RequestParam(value = "units") Double units, @RequestParam(value = "type") Drink.DrinkType type) {
+		Drink newSavedDrink = new Drink(name, volume, alc_content, units, false, type);
 		drinkRepository.save(newSavedDrink);
 		return newSavedDrink;
 	}
-	
-	@GetMapping(path="/all_saved_drinks")
+
+	@GetMapping(path = "/all_saved_drinks")
 	public @ResponseBody Iterable<Drink> fetchSaveddrinks() {
 		return drinkRepository.findByIsDefault(false);
 	}
-	
-	@GetMapping(path="/all_default_drinks")
+
+	@GetMapping(path = "/all_default_drinks")
 	public @ResponseBody Iterable<Drink> fetchDefaultDrinks() {
 		return drinkRepository.findByIsDefault(true);
 	}
-	
-	@GetMapping(path="/all_entries")
+
+	@GetMapping(path = "/all_entries")
 	public @ResponseBody Iterable<DrinkEntry> fetchAllEntries() {
 		return drinkEntryRepository.findAll();
 	}
-	
+
 	@GetMapping(path = "/admin_page")
 	public String adminsivu() {
 		return "admin.html";
 	}
-	
-	@RequestMapping(value = "/add_entry", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/add_entry", method = RequestMethod.POST)
 	public @ResponseBody String addEntry(HttpServletRequest request, @RequestBody DrinkEntry[] entries) {
-		for (DrinkEntry de : entries ) {
+		for (DrinkEntry de : entries) {
 			System.out.println(de.getDrink());
 			System.out.println(de.getDrink().getDrinkId());
 			de.setDrink(drinkRepository.findById(de.getDrink().getDrinkId()));
 			drinkEntryRepository.save(de);
-		}	
+		}
 		return "Added: " + entries;
-	}	
-	
-	@RequestMapping(value = "/delete_entry", method=RequestMethod.POST)
-		public @ResponseBody String deleteEntry(HttpServletRequest request, @RequestParam(value="drink_entry_id") String drink_entry_id) {
-			long idLong = Long.parseLong(drink_entry_id);
-			drinkEntryRepository.deleteById(idLong);	
+	}
+
+	@RequestMapping(value = "/delete_entry", method = RequestMethod.POST)
+	public @ResponseBody String deleteEntry(HttpServletRequest request,
+			@RequestParam(value = "drink_entry_id") String drink_entry_id) {
+		long idLong = Long.parseLong(drink_entry_id);
+		drinkEntryRepository.deleteById(idLong);
 		return "Deleted: " + drink_entry_id;
 	}
-	
-	@RequestMapping(value = "/edit_entry", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/edit_entry", method = RequestMethod.POST)
 	public @ResponseBody String editEntry(HttpServletRequest request, @RequestBody DrinkEntry entry) {
 		drinkEntryRepository.save(entry);
-	return "Updated: " + entry;
-}
-	
+		return "Updated: " + entry;
+	}
+
 }
--- src/main/java/info/jessetaina/alkkispro/model/Drink.java
+++ src/main/java/info/jessetaina/alkkispro/model/Drink.java
@@ -1,6 +1,8 @@
 package info.jessetaina.alkkispro.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -8,7 +10,14 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 public class Drink {
-	
+
+	public enum DrinkType {
+		MILD,
+		WINE,
+		LIQUEUR,
+		BOOZE
+	}
+
 	@Id
 	@GeneratedValue (strategy=GenerationType.AUTO)
 	private long drinkId;
@@ -27,19 +36,24 @@ public class Drink {
 	
 	@NotNull
 	private boolean isDefault;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private DrinkType type;
 	
 	private String icon;
 	
 	public Drink() {};
 	
 	public Drink(@NotNull String drink_name, @NotNull double volume, @NotNull double alc_content,
-			@NotNull double units, @NotNull boolean is_default) {
+			@NotNull double units, @NotNull boolean is_default, @NotNull DrinkType type) {
 		super();
 		this.drinkName = drink_name;
 		this.volume = volume;
 		this.alcContent = alc_content;
 		this.units = units;
-		this.isDefault = is_default; 
+		this.isDefault = is_default;
+		this.type = type;
 	}
 
 	public long getDrinkId() {
@@ -90,6 +104,14 @@ public class Drink {
 		this.isDefault = isDefault;
 	}
 
+	public DrinkType getType() {
+		return type;
+	}
+
+	public void setType(DrinkType type) {
+		this.type = type;
+	}
+
 	public String getIcon() {
 		return icon;
 	}
@@ -101,6 +123,6 @@ public class Drink {
 	@Override
 	public String toString() {
 		return "Drink [drinkId=" + drinkId + ", drinkName=" + drinkName + ", volume=" + volume + ", alcContent="
-				+ alcContent + ", units=" + units + ", isDefault=" + isDefault + "]";
+				+ alcContent + ", units=" + units + ", type=" + type + ", isDefault=" + isDefault + "]";
 	}
 }
